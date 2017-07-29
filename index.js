eb38dbd953c804803f4e88bd708c220f938bc493
marciodasilva <marciodasilva@hotmail.com>
2017-07-29 14:12:04 -0700

    initial google auth

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 /**
  * Created by marciodasilva on 7/28/17.
  */
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientID,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'Delta' });
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
+//app.listen(PORT);
+app.listen(PORT, function() {
+  console.log("Server Started on port:", PORT);
 });
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT);
-
